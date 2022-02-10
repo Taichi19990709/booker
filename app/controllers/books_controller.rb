302c63f26fbdc88f95ffff4a3374d397fe2452f3
watanabe <iamogroman@gmail.com>
2022-02-11 00:42:12 +0900

    'new'

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,20 +7,17 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
-  def new
-    @book = Book.new
-  end
-
   def show 
     @book = Book.find(params[:id])
   end
   
   def create 
-    book = Book.new(book_params)
-    if book.save
-      redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id),notice:"Book was successfully created."
     else 
-      redirect_to new_book_path
+      @books = Book.all
+      render :index
     end
   end
 
@@ -29,9 +26,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id),notice:"Book was successfully updated."
+    else
+      render :edit
+    end
   end
 
   def destroy
